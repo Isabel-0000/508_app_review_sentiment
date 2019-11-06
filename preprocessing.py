24679319ac00d01065ddc137c3f3a7800196a70f
isabel-0000 <blueribbonij02@gmail.com>
2019-11-06 13:53:39 -0800

    produces list of all non-stop words in a file

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,6 @@
 import sys
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 def get_lines_from_file(filename):
    line_list = []
@@ -9,8 +11,24 @@ def get_lines_from_file(filename):
          line = fp.readline()
    return line_list
 
+def remove_stop(line, stopwords):
+   words = word_tokenize(line);
+   words_no_stops = []
+   for word in words:
+      if not word in stopwords:
+         words_no_stops.append(word)
+   return words_no_stops
+
+def remove_all_stop(line_list):
+   stop_words = set(stopwords.words('english'))
+   all_non_stop = []
+   for line in line_list:
+      all_non_stop.extend(remove_stop(line, stop_words))
+   return all_non_stop
+
 def main():
    line_list = get_lines_from_file(sys.argv[1])
-   print(line_list)
+   print(remove_all_stop(line_list))
+
 if __name__ == "__main__":
    main() 
